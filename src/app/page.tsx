7cb02f8ef10abca22019ad7b299a3e49114d982b
DiegoMieles <juandimieles99@gmail.com>
2025-03-17 17:37:09 -0500

    welcome Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Chatbot from "./components/Chatbot";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <Chatbot />
-    </main>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white text-black p-6">
+    <h1 className="text-4xl font-bold mb-4">Página del Consulado Peruano</h1>
+    <p className="text-lg text-center max-w-2xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis 
+      egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel.
+    </p>
+  </main>
   );
 }
